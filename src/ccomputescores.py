f07294ddd8d2d399db057ee0c173ed4ff0d23e74
Mark Stoehr <stoehr.mark@gmail.com>
2013-07-25 11:26:30 -0400

    correctly saves model-testing output now

--- src/ccomputescores.py
+++ src/ccomputescores.py
@@ -17,33 +17,41 @@ def get_utterance_speaker(s):
 def main(args):
     """
     """
-    log_ratio_filter = np.load('%s_filter.npy' % args.o)
-    log_ratio_const = np.load('%s_const.npy' % args.o)
-    bgd_response = np.load('%s_bgd_response.npy' % args.o)
-    model_utterance_tag,model_speaker_id = get_utterance_speaker(args.o)
-    M = np.load('%s_meta.npy' %args.o).astype(int)
+    log_ratio_filter = np.load('%s_filter.npy' % args.m)
+    log_ratio_const = np.load('%s_const.npy' % args.m)
+    bgd_response = np.load('%s_bgd_response.npy' % args.m)
+    model_utterance_tag,model_speaker_id = get_utterance_speaker(args.m)
+    M = np.load('%s_meta.npy' %args.m).astype(int)
 
     fls = open(args.d,'r').read().strip().split('\n')
+
     for fl_id, fl in enumerate(fls):
-        X = np.load("%s_E.npy" % fl).reshape(*((X.shape[0],) + 
-                                               X.shape[1:]))
-        fl_utterance_tag,fl_speaker_id = get_utterance_speaker(fl)
         meta = np.loadtxt("%s_meta.txt" % fl).astype(int)
+
+        X = np.load("%s_E.npy" % fl).reshape(*((meta[-1][1],) + 
+                                               log_ratio_filter.shape[1:]))
+
+        fl_utterance_tag,fl_speaker_id = get_utterance_speaker(fl)
         
-        W = np.zeros((np.max(meta[1]-meta[0]) + 2*np.max(M[1]-M[0]),X.shape[1]))
+        
         word_scores = ()
-        score_meta = np.zeros((meta.shape[0],6))
+        score_meta = np.zeros((meta.shape[0],M.shape[0],6))
+
         for cur_model_id, cur_model_terms in enumerate(M):
-            model_length = end-start
             start,end,model_word_id = cur_model_terms
+            model_length = end-start
+
             cur_log_ratio_filter = log_ratio_filter[start:end]
             cur_log_ratio_const = log_ratio_const[start:end]
-            cur_bgd_response = np.til(bgd_response[start:end],
+            cur_bgd_response = np.tile(bgd_response[start:end],
                                       model_length).reshape((model_length,
                                                              model_length))
-            model_product = np.dot(X,cur_log_ratio_filter.T)
 
+            model_product = np.dot(X,cur_log_ratio_filter.T)
+            W = np.zeros((np.max(meta[1]-meta[0]) + 2*np.max(M[1]-M[0]),
+                          cur_bgd_response.shape[1]))
             W[:model_length] = cur_bgd_response
+
             for fl_entry_id, target_word_meta in enumerate(meta):
                 target_start,target_end,target_word_id = target_word_meta
                 target_length = target_end-target_start
@@ -57,24 +65,25 @@ def main(args):
                 max_idx = - window_radius
                 for sum_idx in xrange(1-window_radius,window_radius):
                     value = np.sum(np.diagonal(W,sum_idx-model_length) + cur_log_ratio_const)
-                     if value > max_value:
-                         max_idx = sum_idx
-                         max_value = value
+                    if value > max_value:
+                        max_idx = sum_idx
+                        max_value = value
                 
-                scores_meta[fl_entry_id,0] = target_word_id
-                scores_meta[fl_entry_id,1] = model_speaker_id
-                scores_meta[fl_entry_id,2] = word_speaker_id
-                scores_meta[fl_entry_id,3] = max_value
-                scores_meta[fl_entry_id,4] = max_idx
-                scores_meta[fl_entry_id,5] = model_word_id
+                score_meta[fl_entry_id,cur_model_id,0] = target_word_id
+                score_meta[fl_entry_id,cur_model_id,1] = fl_speaker_id
+                score_meta[fl_entry_id,cur_model_id,2] = model_speaker_id
+                score_meta[fl_entry_id,cur_model_id,3] = model_word_id
+                score_meta[fl_entry_id,cur_model_id,4] = max_idx
+                score_meta[fl_entry_id,cur_model_id,5] = max_value
 
-        np.save('%s_scores_meta.npy' % fl,
-                 scores_meta)
+
+        np.save('%s/%s_score_meta_model_utt_%s.npy' % (args.o,fl_utterance_tag,model_utterance_tag),
+                 score_meta)
             
             
 
 if __name__=="__main__":
-        parser = argparse.ArgumentParser("""
+    parser = argparse.ArgumentParser("""
         Takes in a template model and a list of locations for where
 data files can be found and then 
 """)
